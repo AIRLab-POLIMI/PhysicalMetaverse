e9d2bf3882d5872a3f69316ba5fed2c132b76c33
TizioMaurizio <TizioMaurizio>
2023-09-25 19:07:16 +0200

    more work on qr

---
+++ Python/MultipleQRDetectRealTestOffline.py
@@ -0,0 +1,179 @@
+
+import numpy as np
+from pyzbar.pyzbar import decode
+import cv2
+from networkStuff.constants import *
+import traceback
+
+#CAMERA = "webcam" #"webcam", "remote", "virtual"
+
+SHOW = True
+
+def loop(vid):
+    global frame
+    success, frame = vid.read()
+    msg = []
+    #downscale frame to half
+    frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+    for barcode in decode(frame):
+        text = barcode.data.decode('utf-8')
+        text=str(text)
+        color=(0,0,255)
+        polygon_Points = np.array([barcode.polygon], np.int32)
+        polygon_Points=polygon_Points.reshape(-1,1,2)
+        rect_Points= barcode.rect
+        if SHOW:
+            cv2.polylines(frame,[polygon_Points],True,color, 3)
+            cv2.putText(frame, barcode.data.decode('utf-8') , (rect_Points[0],rect_Points[1]), cv2.FONT_HERSHEY_PLAIN, 0.9, color, 2)
+
+        #draw the 4 points as big dots
+        #for point in polygon_Points:
+        #    cv2.circle(frame, (point[0][0], point[0][1]), 10, (0, 0, 255), -1)
+        ##draw the first and third point in green from polygon_Points
+        #cv2.circle(frame, (polygon_Points[0][0][0], polygon_Points[0][0][1]), 10, (0, 255, 0), -1)
+        #cv2.circle(frame, (polygon_Points[2][0][0], polygon_Points[2][0][1]), 10, (0, 255, 0), -1)
+        try:
+            rectangle_diagonal = np.sqrt((polygon_Points[0][0][0] - polygon_Points[2][0][0]) ** 2 + (polygon_Points[0][0][1] - polygon_Points[2][0][1]) ** 2)
+            #other diagonal
+            other_rectangle_diagonal = np.sqrt((polygon_Points[1][0][0] - polygon_Points[3][0][0]) ** 2 + (polygon_Points[1][0][1] - polygon_Points[3][0][1]) ** 2)
+            #draw rectangle diagonal
+            if SHOW:
+                cv2.line(frame, (polygon_Points[0][0][0], polygon_Points[0][0][1]), (polygon_Points[2][0][0], polygon_Points[2][0][1]), (255, 0, 0), 2)
+                cv2.line(frame, (polygon_Points[1][0][0], polygon_Points[1][0][1]), (polygon_Points[3][0][0], polygon_Points[3][0][1]), (255, 0, 0), 2)
+            #rectangle center
+            rect_center = (int((polygon_Points[0][0][0] + polygon_Points[2][0][0]) / 2), int((polygon_Points[0][0][1] + polygon_Points[2][0][1]) / 2))
+            #draw
+            if SHOW:
+                cv2.circle(frame, rect_center, 10, (255, 0, 0), -1)
+            ##print("depth frame center value: " + str(depthFrameCenterValue))
+            #print("rectangle height: " + str(rectangle_diagonal))
+            import math
+
+            # Diagonal length of the square in pixels
+            diagonal_length_pixels = rectangle_diagonal  # Replace this with the actual measurement
+            other_diagonal_length_pixels = other_rectangle_diagonal
+            length_to_measure = (diagonal_length_pixels + other_diagonal_length_pixels) / 2
+
+            # Focal length of the camera in millimeters
+            focal_length_mm = 4.81
+
+            # Actual size of the square in the real world (e.g., in meters)
+            actual_square_size_meters = 0.2  # Replace this with the actual measurement
+
+            # Calculate the angular size in radians
+            angular_size_rad = 2 * math.atan(length_to_measure / (2 * focal_length_mm))
+
+            # Calculate the distance using the formula
+            distance_meters = (actual_square_size_meters / 2) / math.tan(angular_size_rad / 2)
+            distance_meters = distance_meters*100* 33/13
+            #print("Distance from camera to square: {:.2f} meters".format(distance_meters))
+            distance_meters *= 100
+            #send qr x,y, size
+            #append key
+            #MSG FORMAT: [barcode, x, y, size(diagonal)]
+            try:
+                currMsg = STATION_KEY + str([int(barcode.data), rect_center[0], rect_center[1], int(distance_meters)]).encode()
+                msg += [currMsg]
+            except:
+                #DIRTY FIX TO USE INVALID QR CODES
+                if ACCEPT_INVALID_QR:
+                    currMsg = STATION_KEY + str([int(1), rect_center[0], rect_center[1], int(distance_meters)]).encode()
+                else:
+                    currMsg = STATION_KEY + str([int(-1), rect_center[0], rect_center[1], int(distance_meters)]).encode()
+                msg += [currMsg]
+            #udp send socket
+        except:
+            print(traceback.print_exc())
+    
+    
+    #aggregate all messages into one
+    stringMsg = b''
+    for m in msg:
+        stringMsg += m
+    cv2.imshow("Image", frame)
+    cv2.waitKey(1)
+        
+    #if stringMsg != b'':
+        #print("send qr " + str(stringMsg))
+        #connection.send("", stringMsg)
+
+#socket sock
+#import socket
+#import traceback
+
+#UDP
+#DEST_IP = "127.0.0.1"
+#DEST_PORT = 25666
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#sock.bind(('127.0.0.1', 25667))
+
+#set timeout
+#sock.settimeout(5)
+
+#STATION_KEY = b'\xc3'
+
+#def QrFromFile():
+#    global qr_png
+#    qr_png = cv2.imread("qrcode.png")
+#    return qr_png
+
+#from PIL import ImageGrab
+#
+#def VirtualQR():
+#    #frame equal to current pc screen
+#    global frame
+#    frame = np.array(ImageGrab.grab(bbox=(0,0,1920,1080)))
+#    #smaller
+#    frame = cv2.resize(frame, (0,0), fx=0.4, fy=0.4)
+#
+#    return frame
+
+#def QrFromUdp():
+#    #receive a list from udp containing width, height, and frame
+#    global frame, sock
+#    try:
+#        data, addr = sock.recvfrom(65536)
+#        data, addr = sock.recvfrom(65536)
+#        #print
+#        #print("RECEIVED MSG " + str(data))
+#        data = np.frombuffer(data, dtype=np.uint8)
+#        frame = cv2.imdecode(data, 1)
+#        return frame
+#    except:
+#        print("No image received")
+#        return frame
+
+import time
+
+ACCEPT_INVALID_QR = True
+vid = None
+
+def start():
+    global vid
+    print("start")
+    #Scanning QR Code from Camera Feed
+    vid = cv2.VideoCapture(0)
+    while True:
+        loop(vid)
+
+start()
+"""
+while True:
+    if CAMERA == "webcam":
+        success, frame = vid.read()
+    elif CAMERA == "remote":
+        frame = QrFromUdp()
+    elif CAMERA == "virtual":
+        frame = VirtualQR()
+    else:
+        print("INVALID CAMERA")
+        break
+    frame = detectQR()
+    cv2.imshow("Video", frame)
+    cv2.waitKey(1)
+    #sleep 0.05
+    #time.sleep(0.05)
+
+"""
+
+  
---
+++ Python/main_predigital.py
@@ -0,0 +1,188 @@
+import queue
+import time
+from networkStuff.constants import *
+from networkStuff.connection import Connection
+import multiprocessing
+import DepthAICamera
+
+WINDOWS = 1
+
+if not WINDOWS:
+    import Jetson.GPIO as GPIO
+
+    # PIN SETUP
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(setup_pin, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(connection_pin, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(third_pin, GPIO.OUT, initial=GPIO.LOW)
+
+# LIDAR SETTINGS
+LIDAR_TOLERANCE = 50
+LIDAR_TIMEOUT_INVALIDATE = 3 # after this amount of invalid or missing readings, the stored value gets invalidated
+LIDAR_MAX_DIST_INVALIDATE = 6000 # maximum distance, set to 0 if greater
+
+# GYRO SETTINGS
+GYRO_TOLERANCE = 0.75
+
+# ENABLE/DISABLE SENSORS
+LIDAR_ENABLED = 0
+GYRO_ENABLED = 0
+POSE_D_ENABLED = 0
+CONTROLLER_ENABLED = 0
+CAMERA_ENABLED = 1
+
+#Enable/disable display output
+POSE_SCREENLESS_MODE = 1
+
+if not WINDOWS:
+    #LIGHT UP LED WHEN SETUP STARTS
+    GPIO.output(setup_pin, GPIO.HIGH)
+
+connection = Connection()
+
+if CAMERA_ENABLED:
+    import DepthAICamera as camera #not a class sorry
+    camera.start()
+connection.set_camera_ready(True)
+
+if LIDAR_ENABLED:
+    from Lidar import Lidar
+    lidar = Lidar()
+    lidarQueue = multiprocessing.Queue(maxsize=100)
+connection.set_lidar_ready(True)
+
+
+if GYRO_ENABLED:
+    from GyroSerial import GyroSerial
+    gyro = GyroSerial()
+connection.set_gyro_ready(True)
+
+if POSE_D_ENABLED:
+    from Pose_detect_new import PoseDetector
+    pose = PoseDetector()
+connection.set_pose_ready(True)
+
+if not WINDOWS:
+    GPIO.output(setup_pin, GPIO.LOW)
+
+class Main:
+    def __init__(self):
+        self.lidar_process = None
+        self.gyro_process = None
+    def setup(self):
+        # setting up the network connection
+
+        if not WINDOWS:
+            GPIO.output(connection_pin, GPIO.HIGH)
+
+        connection.setup()
+
+        if not WINDOWS:
+            GPIO.output(connection_pin, GPIO.LOW)
+
+        #connection.set_lidar_queue(lidarQueue)
+
+        if LIDAR_ENABLED:
+            print("setting up lidar...")
+
+            
+            lidar.sensor.stop()
+            #clean input
+            #lidar.sensor.clean_input()
+            #lidar.print_status()
+            self.lidar_process = multiprocessing.Process(target=lidar.update_measurements,
+                                                   args=[lidarQueue, LIDAR_TOLERANCE, LIDAR_TIMEOUT_INVALIDATE,
+                                                         LIDAR_MAX_DIST_INVALIDATE, connection])
+            self.lidar_process.start()
+
+
+        if GYRO_ENABLED:
+            print("setting up serial communication...")
+            self.gyro_process = multiprocessing.Process(target=gyro.start_update, args=[GYRO_TOLERANCE, connection])
+            self.gyro_process.start()
+
+
+        if POSE_D_ENABLED:
+            print("setting up pose detection...")
+            #pose_d_process = multiprocessing.Process(target=pose.loop, args=[connection, POSE_SCREENLESS_MODE])
+            #pose_d_process.start()
+    
+        if CAMERA_ENABLED:
+            print("setting up camera...")
+            #camera_process = multiprocessing.Process(target=camera.main, args=[connection])
+            #camera_process.start()
+
+        if CONTROLLER_ENABLED:
+            import Controller
+            controller_process = multiprocessing.Process(target=Controller.main, args=[])
+            controller_process.start()
+
+        print("Setup COMPLETED =)")
+
+    def loop(self):
+
+        if not WINDOWS:
+            GPIO.output(third_pin, GPIO.HIGH)
+
+        i = 0
+        try:
+            connection_process = multiprocessing.Process(target=connection.loop, args=[])
+            connection_process.start()
+            while connection_process.is_alive():
+                if CAMERA_ENABLED:
+                    DepthAICamera.loop(connection)
+                if POSE_D_ENABLED:
+                    pose.getMeasure(connection, POSE_SCREENLESS_MODE)
+
+            self.restart()
+                #connection.loop()
+        except ConnectionError:
+            self.restart()
+
+        except KeyboardInterrupt:
+            self.destroy_connections()
+            if not WINDOWS:
+                GPIO.output(setup_pin, GPIO.LOW)
+                GPIO.output(connection_pin, GPIO.LOW)
+                GPIO.output(third_pin, GPIO.LOW)
+
+    def destroy_connections(self):
+        print("\n\n")
+        print("REMAINING PROCESSES: "+ str(multiprocessing.active_children()))
+        print("\n\n")
+        #for each process close
+        for process in multiprocessing.active_children():
+            print("TERMINATING: "+ str(process))
+            process.kill()
+        
+        print("\n\n")
+        print("REMAINING PROCESSES: "+ str(multiprocessing.active_children()))
+        print("\n\n")
+
+        connection.close_all_connections()
+        connection.cleanup()
+
+    def restart(self):
+        self.destroy_connections()
+
+        if not WINDOWS:
+            GPIO.output(third_pin, GPIO.LOW)
+
+        #if LIDAR_ENABLED:
+        #    self.lidar_process.terminate()
+#
+        #if GYRO_ENABLED:
+        #    self.gyro_process.terminate()
+        #connection.retry_connection()
+        self.setup()
+        self.loop()
+
+    def close(self):
+        #pose.close()
+        connection.cleanup()
+
+if __name__ == '__main__':
+    main = Main()
+    main.setup()
+    main.loop()
+    main.close()
